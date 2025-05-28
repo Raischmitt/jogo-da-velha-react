4db2c0f611e3ca3ff9a5d07a953d4bf9c5a39a9d
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-05-27 21:56:20 -0300

    iniciando os primeiros componentes

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleClick = () => {
+        setIsEditing(true);
+    }
+    return (
+        <li>
+            <span className="player">
+                {isEditing && <span className="player-name">{name} </span>}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>
+                Edit
+            </button>
+        </li>
+
+    );
+
+};
